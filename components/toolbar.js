23bf7f31cf3dcf98ae10e5aa1716eff83e8a4fa8
Kacper <hernackikacper@gmail.com>
2021-05-05 20:20:42 +0200

    image

--- components/toolbar.js
+++ components/toolbar.js
@@ -2,18 +2,24 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/Toolbar.module.css';
 // icons
-import TwitterIcon from '@material-ui/icons/Twitter';
-import GitHubIcon from '@material-ui/icons/GitHub';
+
+import Image from 'next/image';
 
 export const Toolbar = ({ content }) => {
   const router = useRouter();
 
   return (
     <div className={content ? styles.mainScrolled : styles.main}>
+      <Image
+        className={styles.logoImage}
+        src={`/images/test.png`}
+        alt="Picture of the author"
+        width={50}
+        height={50}
+      />
       <div className={styles.logo} onClick={() => router.push('/')}>
         The<span>Devs</span>Universe
       </div>
-     
     </div>
   );
 };
